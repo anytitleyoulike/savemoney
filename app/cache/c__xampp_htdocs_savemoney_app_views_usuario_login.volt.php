fde7554c3e96fa1e9a148831e68b70522144a51c
Marcello <maarc.hen@gmail.com>
2014-12-12 17:43:21 -0400

    atualizando tela de login

--- app/cache/c__xampp_htdocs_savemoney_app_views_usuario_login.volt.php
+++ app/cache/c__xampp_htdocs_savemoney_app_views_usuario_login.volt.php
@@ -23,27 +23,33 @@
             <div class="col-md-offset-2 col-lg-8">
                 <div class="well">     
                     <form name='login' class="form-horizontal" method="post" action="/savemoney/usuario/login">
-                        <fieldset>
-                            <legend>Login</legend>
-                            <div class="form-group">
-                                <label for="inputEmail" class="col-lg-2 control-label">Email</label>
-                                <div class="col-lg-10">
-                                    <input type="email" class="form-control" name="email" placeholder="Email">
+                        <h1>Savemoney</h1>
+                        <div class="panel panel-default">
+                            <div class="panel-heading">Login</div>
+                                <div class="panel-body">
+                                    <fieldset>
+                                        <div class="form-group">
+                                            <label for="inputEmail" class="col-lg-2 control-label">Email</label>
+                                            <div class="col-lg-10">
+                                                <input type="email" class="form-control" name="email" placeholder="Email">
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="inputPassword" class="col-lg-2 control-label">Senha</label>
+                                            <div class="col-lg-10">
+                                                <input type="password" class="form-control" name="senha" placeholder="Senha">
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="col-lg-10 col-lg-offset-2">
+                                                <button class="btn btn-default">Cancel</button>
+                                                <button type="submit" class="btn btn-primary">Entrar</button>
+                                            </div>
+                                        </div>
+                                    </fieldset>
                                 </div>
                             </div>
-                            <div class="form-group">
-                                <label for="inputPassword" class="col-lg-2 control-label">Senha</label>
-                                <div class="col-lg-10">
-                                    <input type="password" class="form-control" name="senha" placeholder="Senha">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="col-lg-10 col-lg-offset-2">
-                                    <button class="btn btn-default">Cancel</button>
-                                    <button type="submit" class="btn btn-primary">Login</button>
-                                </div>
-                            </div>
-                        </fieldset>
+                        </div>
                     </form>
                 </div>
             </div>
